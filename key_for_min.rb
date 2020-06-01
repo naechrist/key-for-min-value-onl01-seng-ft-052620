59565718e12f1275f2afd30c5bbc9318eff49490
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-01 03:06:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,12 @@
 
 
 def key_for_min_value(name_hash)
-  lowest_value = 1000
-  result = ""
-name_hash.each do |name, hash|
-  if hash <  
-    puts name 
+  lowest_value = 0
+  lowest_name = nil
+name_hash.each do |key, value|
+  if lowest_value == 0 || value < lowest_value
+    lowest_value = value
+    lowest_key = key
     
     end
 
