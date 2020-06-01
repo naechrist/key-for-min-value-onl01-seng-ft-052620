2962bd2354963cbc6edd1f8c3660e7a6b7d04f41
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-01 03:00:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,14 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-lowest_value = 0 
+
 
 def key_for_min_value(name_hash)
-key_for_min_value.each do |name, hash|
-  if 
+  lowest_value = 1000
+  result = ""
+name_hash.each do |name, hash|
+  if hash <  
+    puts name 
     
     end
 
